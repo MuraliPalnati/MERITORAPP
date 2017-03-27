0b0561ed34a2a181cebd95b2e196de65f5bfeba1
Kanumuri SriRaviTeja <Kanumuri SriRaviTeja@KANUMURISRIRAVI>
2017-03-28 01:03:43 +0530

    application_error

--- MERITOR.StockRoom.Web/Global.asax.cs
+++ MERITOR.StockRoom.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using MERITOR.StockRoom.Web.Areas.Manage.Handler;
+﻿using log4net;
+using MERITOR.StockRoom.Web.Areas.Manage.Handler;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -6,7 +7,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-
 
 namespace MERITOR.StockRoom.Web
 {
@@ -17,6 +17,46 @@ namespace MERITOR.StockRoom.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception GlobalException = Server.GetLastError();
+            ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            logger.Error("Gloabl Error Handler --" + GlobalException.Message);
+            Server.ClearError();
+            HttpException httpException = GlobalException as HttpException;
+            //Response.Redirect("/Error/GlobalError");
+            //Response.Write(httpException.Message);
+            if (httpException != null)
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 400:
+                        // BadRequest
+                        Response.Redirect("Error/BadRequest");
+                        break;
+                    case 401:
+                        // Unauthorized
+                        Response.Redirect("/Error/Unauthorized");
+                        break;
+                    case 404:
+                        // page not found
+                        Response.Redirect("/Error/NotFound");
+                        break;
+                    case 500:
+                        // InternalServer error
+                        Response.Redirect(" /Error/InternalServer");
+                        break;
+                    default:
+                        Response.Redirect(" /Error/GlobalError");
+                        break;
+                }
+            }
+            else
+            {
+                Response.Redirect(" /Error/GlobalError");
+            }
         }
     }
 }
