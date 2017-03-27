29bdbb22e731836b966cd62a42b0474a824227d7
Kanumuri SriRaviTeja <Kanumuri SriRaviTeja@KANUMURISRIRAVI>
2017-03-28 00:22:30 +0530

    global error

--- MERITOR.StockRoom.Web/Controllers/ErrorController.cs
+++ MERITOR.StockRoom.Web/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,27 +9,41 @@ namespace MERITOR.StockRoom.Web.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILog logger = null;
+        public ErrorController()
+        {
+            logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
 
         // 400 Error
         public ActionResult BadRequest()
         {
+            logger.Error("BadRequest");
             return View();
         }
 
         // 401 Error
         public ActionResult Unauthorized()
         {
+            logger.Error("Unauthorized");
             return View();
         }
         // 404 Error
         public ActionResult NotFound()
         {
+            logger.Error("NotFound");
             return View();
         }
 
         // 500 Error 
         public ActionResult InternalServer()
         {
+            logger.Error("InternalServer");
+            return View();
+        }
+        public ActionResult GlobalError()
+        {
+            logger.Error("GlobalError");
             return View();
         }
     }
